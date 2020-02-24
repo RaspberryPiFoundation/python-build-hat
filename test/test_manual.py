ec02f8228bbebaf71b3fc7749ee9fc06c3d5f5d9
Max Conway <mjc@kynesim.co.uk>
2020-02-24 16:00:20 +0000

    Added new tests based on actually using device

--- test/test_manual.py
+++ test/test_manual.py
@@ -12,7 +12,7 @@ class GeneralTestCase(unittest.TestCase):
 		assert {'info', 'port'}.issubset(dir(hub))
 		assert isinstance(hub.info(), dict)
 		assert len(hub.info())>=2
-		assert {'hardware_revision', 'firmware_revision'}.issubset(hub.info().keys())
+		assert {'hardware_revision', 'device_uuid'}.issubset(hub.info().keys()) # From real hub
 
 	def test_port_types(self):
 		ports = [hub.port.A, hub.port.B, hub.port.C, hub.port.D, hub.port.F]
@@ -53,6 +53,36 @@ class PortDetachedTestCase(unittest.TestCase):
 	def test_port_device(self):
 		assert hub.port.B.device is None
 
+# These tests were designed on the hub itself
+class HubFunctionalityTestCase(unittest.TestCase):
+	def test_hub_functionality(self):
+		{'BT_VCP', 'Image', 'USB_VCP', 'battery', 'ble', 'bluetooth', 'button', 'display', 'firmware', 'info', 'led', 'motion', 'port', 'power_off', 'sound', 'status', 'supervision', 'temperature', 'text'}.issubset(dir(hub))
+		assert {'hardware_revision', 'device_uuid'}.issubset(hub.info().keys())
+
+	def test_port_functionality(self):
+		ports = [hub.port.A, hub.port.B, hub.port.C, hub.port.D, hub.port.F]
+		random.shuffle(ports)
+		for P in ports:
+			assert {'callback', 'device', 'info', 'mode', 'motor', 'pwm'}.issubset(dir(P)) 
+			assert isinstance(P.info(), dict)
+			assert {'type', 'fw_version', 'hw_version', 'modes', 'combi_modes'}.issubset(P.info().keys())
+
+	# Motor must be connected to port A
+	def test_motor_A(self):
+		assert {'BUSY_MODE', 'BUSY_MOTOR', 'EVENT_COMPLETED', 'EVENT_INTERRUPTED', 'FORMAT_PCT', 'FORMAT_RAW', 'FORMAT_SI', 'PID_POSITION', 'PID_SPEED', 'STOP_BRAKE', 'STOP_FLOAT', 'STOP_HOLD', 'brake', 'busy', 'callback', 'default', 'float', 'get', 'hold', 'mode', 'pair', 'pid', 'preset', 'pwm', 'run_at_speed', 'run_for_degrees', 'run_for_time', 'run_to_position'}.issubset(dir(hub.motor.A))
+		hub.port.A.motor.run_for_time(1000, 127) # run for 1000ms at maximum clockwise speed
+		hub.port.A.motor.run_for_time(1000, -127) # run for 1000ms at maximum anticlockwise speed
+		hub.port.A.motor.run_for_degrees(180, 127) # turn 180 degrees clockwise at maximum speed
+		hub.port.A.motor.run_for_degrees(720, -127) # Make two rotations anticlockwise at maximum speed
+		hub.port.A.motor.run_to_position(0, 127) # Move to top dead centre at maximum speed (positioning seems to be absolute)
+		hub.port.A.motor.run_to_position(180, 127) # Move to 180 degrees forward of top dead centre at maximum speed
+
+	# Touch sensor must be connected to port B
+	def test_touch_sensor_B(self)
+		assert {'FORMAT_PCT', 'FORMAT_RAW', 'FORMAT_SI', 'get', 'mode', 'pwm'}.issubset(dir(hub.port.B.device))
+		assert hub.port.B.device.get() <= 1 # Without button pressed
+		assert hub.port.B.device.get() >= 0 # At all times
+		assert hub.port.B.device.get() <= 9 # At all times
 
 if __name__ == '__main__':
     unittest.main()
