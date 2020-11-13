662b3ec1fab55aede1001505add9024eb230ee2c
chrisruk <chrisrichardsonuk@gmail.com>
2020-11-13 12:53:43 +0000

    add force and distance sensors

--- buildhat/__init__.py
+++ buildhat/__init__.py
@@ -1,2 +1,4 @@
 from .hat import (Motor,
-                  MotorPair)
+                  MotorPair,
+                  Force,
+                  Distance)
--- buildhat/hat.py
+++ buildhat/hat.py
@@ -36,3 +36,28 @@ class MotorPair(Device):
         self._motora = motora
         self._motorb = motorb
         patchattr(self, motora.pair(motorb._motor))
+
+
+class Force(Device):
+    def __init__(self, port):
+        super().__init__()
+        self._port = getattr(self._instance.port, port)
+        self._device = self._port.device
+        patchattr(self, self._device)
+
+    def get_force(self):
+        return self._device.get()[2]
+
+    def get_pressed(self):
+        return self._device.get()[1] == 1
+
+
+class Distance(Device):
+    def __init__(self, port):
+        super().__init__()
+        self._port = getattr(self._instance.port, port)
+        self._device = self._port.device
+        patchattr(self, self._device)
+
+    def get_distance(self):
+        return self._device.get()[0]
