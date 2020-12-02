0a2e19f2879f3f1cfbe61316d166cdfc264d89d3
chrisruk <chrisrichardsonuk@gmail.com>
2020-12-02 14:35:17 +0000

    removing whitespace

--- buildhat/devices.py
+++ buildhat/devices.py
@@ -2,7 +2,6 @@ from build_hat import BuildHAT
 
 class Device:
     """Creates a single instance of the buildhat for all devices to use"""
-
     _instance = None
 
     def __init__(self):
@@ -12,7 +11,6 @@ class Device:
 
 class PortDevice(Device):
     """Device which uses port"""
-
     def __init__(self, port):
         super().__init__()
         self._port = getattr(self._instance.port, port)
