1466c7956fcc43d01e7da545d13188421f741e06
chrisruk <chrisrichardsonuk@gmail.com>
2021-08-19 22:20:51 +0100

    remove c

--- setup.py
+++ setup.py
@@ -12,23 +12,6 @@ LIB_VERSION="0.4.3"
 with open("README.md") as readme:
     long_description = readme.read()
 
-hub_module = Extension('build_hat',
-                       include_dirs = ['include'],
-                       sources = ['src/hubmodule.c',
-                                  'src/uart.c',
-                                  'src/queue.c',
-                                  'src/cmd.c',
-                                  'src/port.c',
-                                  'src/device.c',
-                                  'src/motor.c',
-                                  'src/callback.c'])
-hub_module.define_macros.append(('LIB_VERSION', LIB_VERSION))
-
-# If DEBUG_UART is set, extra commands are added to the hub
-# module to facilite debugging.
-if getenv("DEBUG_UART") == "1":
-    hub_module.define_macros.append(('DEBUG_UART', '1'))
-
 setup(name='build_hat',
       version=LIB_VERSION,
       description='Strawberry library for accessing Shortcake',
@@ -40,5 +23,4 @@ setup(name='build_hat',
       package_data={
           "": ["data/firmware.bin", "data/signature.bin", "data/version"],
       },
-      install_requires=['gpiozero','pyserial'],
-      ext_modules=[hub_module])
+      install_requires=['gpiozero','pyserial'])
