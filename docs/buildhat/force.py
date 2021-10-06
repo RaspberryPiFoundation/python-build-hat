da1ca866dcb98bae32ff0ab61926454f38bd010c
chrisruk <chrisrichardsonuk@gmail.com>
2021-10-06 20:42:40 +0100

    update force example

--- docs/buildhat/force.py
+++ docs/buildhat/force.py
@@ -2,12 +2,12 @@ from signal import pause
 from buildhat import Motor, ForceSensor
 
 motor = Motor('A')
-button = ForceSensor('D')
+button = ForceSensor('D', threshold_force=1)
 
-print("Waiting for button to be pressed and released")
+print("Waiting for button to be pressed fully and released")
 
-button.wait_until_pressed()
-button.wait_until_released()
+button.wait_until_pressed(100)
+button.wait_until_released(0)
 
 motor.run_for_rotations(1)
 
@@ -16,16 +16,12 @@ print("Wait for button to be pressed")
 button.wait_until_pressed()
 motor.run_for_rotations(2) 
 
-def handle_force(force, pressed):
-    print("Force", force, pressed)
+def handle_pressed(force):
+    print("pressed", force)
 
-def handle_pressed():
-    print("pressed")
+def handle_released(force):
+    print("released", force)
 
-def handle_released():
-    print("released")
-
-button.when_force = handle_force
 button.when_pressed = handle_pressed
 button.when_released = handle_released
 pause()
