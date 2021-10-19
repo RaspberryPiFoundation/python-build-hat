cc3d0fdb722350267da8c706125667f873a5fc9c
chrisruk <chrisrichardsonuk@gmail.com>
2021-10-19 10:24:27 +0100

    remove unneeded files

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 python3 -m venv hat_env 
 source hat_env/bin/activate 
-USE_DUMMY_I2C=1 ./setup.py build 
-USE_DUMMY_I2C=1 ./setup.py install
+./setup.py build
+./setup.py install
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source hat_env/bin/activate 
-
-pip3 install psutil
-
-python3 ./test/test_manual.py test/resources/FakeHat
