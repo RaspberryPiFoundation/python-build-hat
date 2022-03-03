85e32ea3a80716c17519cbc47062a42f51bde897
chrisruk <chrisrichardsonuk@gmail.com>
2022-03-03 14:41:29 +0000

    Test Hat object

--- test/hat.py
+++ test/hat.py
@@ -2,7 +2,7 @@ import unittest
 from buildhat import Hat
 
 
-class TestVin(unittest.TestCase):
+class TestHat(unittest.TestCase):
 
     def test_vin(self):
         h = Hat()
@@ -10,6 +10,9 @@ class TestVin(unittest.TestCase):
         self.assertGreaterEqual(vin, 7.2)
         self.assertLessEqual(vin, 8.5)
 
+    def test_get(self):
+        h = Hat()
+        self.assertIsInstance(h.get(), dict)
 
 if __name__ == '__main__':
     unittest.main()
