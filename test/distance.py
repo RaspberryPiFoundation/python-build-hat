8cc616b9154215ba14280466c5ab9c7e9388ed6e
chrisruk <chrisrichardsonuk@gmail.com>
2022-03-03 15:14:14 +0000

    Basic tests for distance sensor

--- test/distance.py
+++ test/distance.py
@@ -1,6 +1,6 @@
 import unittest
 from buildhat import DistanceSensor
-
+from buildhat.exc import PortInUse
 
 class TestDistance(unittest.TestCase):
 
@@ -16,6 +16,15 @@ class TestDistance(unittest.TestCase):
     def test_eyes(self):
         d = DistanceSensor('A')
         d.eyes(100, 100, 100, 100)
+
+    def test_duplicate_port(self):
+        d = DistanceSensor('A')
+        self.assertRaises(PortInUse, DistanceSensor, 'A')
+
+    def test_del(self):
+        d = DistanceSensor('A')
+        del d
+        d = DistanceSensor('A')
 
 if __name__ == '__main__':
     unittest.main()
