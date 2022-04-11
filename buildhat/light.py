71dd6137b8c24c42bb31531f7f93a3589175ae72
mutesplash <49622611+mutesplash@users.noreply.github.com>
2022-04-10 23:20:08 -0400

    Retain brightness setting across reboot

--- buildhat/light.py
+++ buildhat/light.py
@@ -10,7 +10,12 @@ class Light(Device):
     :raises DeviceInvalid: Occurs if there is no light attached to port
     """
     def __init__(self, port):
+        self._brightness = 0
         super().__init__(port)
+
+    def _reset(self):
+        super()._reset()
+        self.brightness(_brightness)
 
     def brightness(self, brightness):
         """
@@ -20,4 +25,5 @@ class Light(Device):
         """
         if not (brightness >= 0 and brightness <= 100) :
             raise LightException("Need brightness arg, of 0 to 100")
+        self._brightness = brightness
         self._write("port {} ; on ; plimit {}\r".format(self.port, brightness / 100.0))
