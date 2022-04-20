0b24fecd6b876ed8db669e758d3b2ba33993fff0
chrisruk <chrisrichardsonuk@gmail.com>
2022-04-20 12:07:34 +0100

    Lint setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from setuptools import setup, Extension
-from os import getenv
+from setuptools import setup
 
 with open("README.md") as readme:
     long_description = readme.read()
@@ -20,14 +19,12 @@ setup(name='buildhat',
       long_description=long_description,
       long_description_content_type="text/markdown",
       author='Raspberry Pi Foundation',
-      author_email = 'web@raspberrypi.org',
-      url = 'https://github.com/RaspberryPiFoundation/python-build-hat',
-      project_urls = {
-        'Bug Tracker': "https://github.com/RaspberryPiFoundation/python-build-hat/issues",
-      },
+      author_email='web@raspberrypi.org',
+      url='https://github.com/RaspberryPiFoundation/python-build-hat',
+      project_urls={'Bug Tracker': "https://github.com/RaspberryPiFoundation/python-build-hat/issues"},
       packages=['buildhat'],
       package_data={
           "": ["data/firmware.bin", "data/signature.bin", "data/version"],
       },
-      python_requires = '>=3.7',
-      install_requires=['gpiozero','pyserial']),
+      python_requires='>=3.7',
+      install_requires=['gpiozero', 'pyserial'])
