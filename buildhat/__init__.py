92bfc70a6197882d14a9c63afe773337eeac92e3
chrisruk <chrisrichardsonuk@gmail.com>
2022-04-20 11:55:02 +0100

    Lint __init__.py

--- buildhat/__init__.py
+++ buildhat/__init__.py
@@ -1,11 +1,11 @@
-from .motors import Motor, MotorPair, PassiveMotor
-from .distance import DistanceSensor
-from .force import ForceSensor
 from .color import ColorSensor
 from .colordistance import ColorDistanceSensor
-from .matrix import Matrix
-from .wedo import TiltSensor, MotionSensor
-from .light import Light
-from .hat import Hat
-from .serinterface import BuildHAT
+from .distance import DistanceSensor
 from .exc import *
+from .force import ForceSensor
+from .hat import Hat
+from .light import Light
+from .matrix import Matrix
+from .motors import Motor, MotorPair, PassiveMotor
+from .serinterface import BuildHAT
+from .wedo import MotionSensor, TiltSensor
