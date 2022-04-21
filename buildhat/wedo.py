d73d2e5b136934d6f2ef32e636614e55b3964b9a
chrisruk <chrisrichardsonuk@gmail.com>
2022-04-21 16:26:49 +0100

    Lint wedo.py

--- buildhat/wedo.py
+++ buildhat/wedo.py
@@ -1,3 +1,5 @@
+"""WeDo sensor handling functionality"""
+
 from .devices import Device
 
 
@@ -7,13 +9,19 @@ class TiltSensor(Device):
     :param port: Port of device
     :raises DeviceError: Occurs if there is no tilt sensor attached to port
     """
+
     def __init__(self, port):
+        """
+        Initialise tilt sensor
+
+        :param port: Port of device
+        """
         super().__init__(port)
         self.mode(0)
 
     def get_tilt(self):
         """
-        Returns the tilt from tilt sensor to object
+        Return the tilt from tilt sensor
 
         :return: Tilt from tilt sensor
         :rtype: tuple
@@ -27,13 +35,19 @@ class MotionSensor(Device):
     :param port: Port of device
     :raises DeviceError: Occurs if there is no motion sensor attached to port
     """
+
     def __init__(self, port):
+        """
+        Initialise motion sensor
+
+        :param port: Port of device
+        """
         super().__init__(port)
         self.mode(0)
 
     def get_distance(self):
         """
-        Returns the distance from motion sensor to object
+        Return the distance from motion sensor
 
         :return: Distance from motion sensor
         :rtype: int
